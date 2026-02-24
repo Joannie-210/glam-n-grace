6194877b4689320e4d112d2467dd5c8633b062cb
Joanna Enoch <pamigold07@gmail.com>
2026-02-24 22:48:57 +0100

    Update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -92,7 +92,7 @@ export default function Homepage() {
 
   return (
    <div className="font-sans min-h-screen overflow-x-hidden">
-      =
+      
       <nav className="bg-white border-b border-gray-100 shadow-sm">
         <div className="flex items-center justify-between px-6 py-4">
          
